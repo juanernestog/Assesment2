92ff43a69234cb58e7ed7cccac516cdea77706b8
JuanE <juanernestogranados@gmail.com>
2022-04-22 18:39:06 -0500

    Created server  skeleton  and  routes to do list

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config(); // Load .env file
+const express = require('express'); // Load Express
+const app = express(); // Create Express app
+mongooose = require('mongoose'); // Load Mongoose
+
+mongooose.connect(process.env.DB_URL); // Connect to MongoDB
+const db = mongooose.connection; // Get Mongoose connection
+db.on('error', console.error.bind(console, 'connection error:')); // Log connection errors
+db.once('open', () => console.log(`connected to mongoose`)); // Log successful connection
+
+app.use(express.json()); // Use JSON
+
+//TO DO: add routes
+//const userRouter = require('./routes/user');
+// app.use('/user', userRouter);
+// const favRouter = require('./routes/fav');
+// app.use('/fav', favRouter);
+// const favListRouter = require('./routes/favList');
+// app.use('/favList', favListRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
