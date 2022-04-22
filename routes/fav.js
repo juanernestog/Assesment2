2bea6b1b8eba716d92b62f4eb6323c348598cde9
JuanE <juanernestogranados@gmail.com>
2022-04-22 18:53:52 -0500

    created  get all favs middleware and created TO DO

--- routes/fav.js
+++ routes/fav.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// TO DO: add routes
+// Get all favs
+router.get('/api/fav/', (req, res) => {
+  Fav.find({}, (err, favs) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(favs);
+  });
+});
+// Get one fav
+// Create one fav
+// Delete one fav
+
 module.exports = router;
