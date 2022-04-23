b18ace83309815ff1773e0ec76d69fad09a47279
JuanE <juanernestogranados@gmail.com>
2022-04-22 20:03:33 -0500

    imported models to routers

--- routes/fav.js
+++ routes/fav.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Fav = require('../models/fav');
 
 // TO DO: add routes
 // Get all fav
@@ -24,11 +25,19 @@ router.get('/api/fav/:id', (req, res) => {
 });
 // Create one fav
 router.post('/api/fav/', (req, res) => {
-  Fav.create(req.body, (err, fav) => {
+  const fav = new Fav({
+    ownerID: req.body.userID,
+    title: req.body.title,
+    description: req.body.description,
+    URL: req.body.URL,
+  });
+  fav.save((err) => {
     if (err) {
       res.send(err);
     }
-    res.json(fav);
+    res.json({
+      message: 'Fav created',
+    });
   });
 });
 // Delete one fav
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
