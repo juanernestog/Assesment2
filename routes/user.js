2bf8350d0dfe58021a3957fec570b2f6231c1396
JuanE <juanernestogranados@gmail.com>
2022-04-22 19:15:58 -0500

    Added auth route

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// TO DO: add auth
+router.get('/auth/local/login', (req, res) => {
+  // on route /auth/local/login
+  const token = jwt.sign(
+    {
+      email: req.params.email,
+      password: req.params.password,
+    },
+    'secret',
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  );
+  res.json({
+    token,
+  });
+});
 
 module.exports = router;
